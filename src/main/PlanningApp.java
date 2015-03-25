befef5a23f9c259423207e5907ee8f75fce171d4
mathias <mathiasgam@hotmail.com>
2015-03-25 21:19:39 +0100

    adding User class adding getUser() and setUser() to planningApp

--- src/main/PlanningApp.java
+++ src/main/PlanningApp.java
@@ -1,5 +1,18 @@
 package main;
 
+import java.util.Collections;
+import java.util.Set;
+
 public class PlanningApp {
+	
+	private Set<User> users;
+	
+	public Set<User> getUsers(){
+		return Collections.unmodifiableSet(users);
+	}
+	
+	public void setUsers(Set<User> users){
+		this.users = users;
+	}
 
 }
--- test/auto/TestInitSystem.java
+++ test/auto/TestInitSystem.java
@@ -1,19 +1,24 @@
 package auto;
 
 import static org.junit.Assert.*;
+import main.PlanningApp;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestInitSystem {
+	
+	PlanningApp planApp;
 
 	@Before
 	public void setUp() throws Exception {
+		planApp = new PlanningApp();
+		
 	}
 
 	@Test
 	public void test() {
-		//TODO add something
+		//TODO do some tests
 	}
 
 }
