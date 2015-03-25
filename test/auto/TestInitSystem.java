16f5e142c07cf4b037ac6662fda1e624d8f4734a
mathias <mathiasgam@hotmail.com>
2015-03-25 20:14:53 +0100

    added the initial files and libaries

---
+++ test/auto/TestInitSystem.java
@@ -0,0 +1,19 @@
+package auto;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestInitSystem {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//TODO add something
+	}
+
+}
