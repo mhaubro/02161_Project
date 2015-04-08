c036cf14fb7585da35050723056ebc26120d3967
mhaubro <martin.haubro@hotmail.com>
2015-04-08 18:57:31 +0200

    Tilføjet tomme test-filer, som skal laves. Ikke det store.

---
+++ test/auto/Planning.java
@@ -0,0 +1,5 @@
+package auto;
+//Skal måske deles i en addPlan og check isPlanned
+public class Planning {
+
+}
---
+++ test/auto/SeeRegisteredTime.java
@@ -0,0 +1,5 @@
+package auto;
+//User story 6. Se registreret tid som medarbejder.
+public class SeeRegisteredTime {
+
+}
