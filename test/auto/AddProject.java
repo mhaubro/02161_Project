d352a6d277f07594ed99153b4b26757301f17766
mathias <mathiasgam@hotmail.com>
2015-04-16 07:58:41 +0200

    merge with addUser

--- test/auto/AddProject.java
+++ test/auto/AddProject.java
@@ -1,5 +1,43 @@
 package auto;
 
+import static org.junit.Assert.*;
+import main.PlanningApp;
+import main.Project;
+import main.User;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class AddProject {
+
+	PlanningApp planApp;
+
+	@Before
+	public void setUp() throws Exception {
+		planApp = new PlanningApp();
+	}
+
+	@Test
+	public void testMain() {
+		
+		// the initial setup for the test.
+		User u = new User("user1", "u1");
+		Project p = new Project("project1", "1234", u);
+		
+		planApp.addUser("user1");
+
+		// adding the project to the planningApp
+		planApp.addProject(p);
+	}
+
+	@Test
+	public void testIsNotSuperUser() {
+
+	}
+
+	@Test
+	public void testSuperDosntAddUser() {
+
+	}
 
 }
