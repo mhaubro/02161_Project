dd77a2687a9148957835755dc61f10ad38841b7c
mathias <mathiasgam@hotmail.com>
2015-05-11 12:51:19 +0200

    added clear planapp

---
+++ UI/main/PlanAppUserInterfaceClean.java
@@ -0,0 +1,49 @@
+package main;
+
+import java.util.Observable;
+
+import exceptions.InputWrongFormatException;
+
+public class PlanAppUserInterfaceClean extends JFrameUserInterface {
+
+	Menu activeMenu;
+
+	String lastInput = "";
+
+	public PlanAppUserInterfaceClean(PlanningApp planApp) {
+		super(planApp);
+		activeMenu = new MainMenu(this);
+		printMenu();
+	}
+
+	@Override
+	public void readInput(String inputString) {
+		try {
+			activeMenu = activeMenu.readInput(inputString);
+			if (activeMenu == null)
+				System.exit(0);
+
+			printMenu();
+
+		} catch (InputWrongFormatException e) {
+			output.setText(activeMenu + "Could not recognize the input: " + inputString + "\n");
+		}
+	}
+
+	private void printMenu() {
+		output.setText(activeMenu.toString());
+	}
+
+	public static void main(String[] args) {
+		PlanningApp planApp = new PlanningApp();
+		JFrameUserInterface UI = new PlanAppUserInterface(planApp);
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		super.update(o, arg);
+
+		printMenu();
+	}
+
+}
